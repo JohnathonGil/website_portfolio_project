cef00cc62257c4edd2cde8c76fdd44ff8792971e
JohnathonGil <jggil@ualberta.ca>
2023-07-01 19:52:33 -0600

    Modified the forms look

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -16,23 +16,20 @@ const Form = () => {
     return (
         <Paper sx={{padding: 4}}>
             <form autoComplete='off' noValidate sx={{ 
-                '& .MuiTextField-root': {
-                    margin: 8,
-                },
                 display: 'flex',
                 flexWrap: 'wrap',
                 justifyContent: 'center',     
             }}
             onSubmit={handleSubmit}>
                 <Typography variant ="h6">Creating a Post</Typography>
-                <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}/> 
-                <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })}/>
-                <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })}/>
-                <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })}/>
+                <TextField name="creator" margin="dense" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}/> 
+                <TextField name="title" margin="dense" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })}/>
+                <TextField name="message" margin="dense" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })}/>
+                <TextField name="tags" margin="dense" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })}/>
                 <div sx={{ 
                     width: '97%',
                     margin: '20px',
-                    marginTop: '10px',
+                    marginTop: '20px',
                 }}><FileBase type = "file"multiple={false} onDone = {({base64}) => setPostData({...postData, selectedFile: base64})}/></div>
                 <Button sx={{  marginBottom: '10px',
                                 marginTop: '10px',
